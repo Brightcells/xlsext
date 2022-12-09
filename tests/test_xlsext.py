48f9290e2fe99df6f30a04f4da1bfab51d28c8f2
huangqimin001 <huangqimin001@ke.com>
2022-12-09 23:51:20 +0800

    :art: Support friendly_name None

--- tests/test_xlsext.py
+++ tests/test_xlsext.py
@@ -10,9 +10,13 @@ class TestXlsExtCommands(object):
     def setup_class(self):
         self.link_location = 'http://a.com'
         self.friendly_name = '测试'
+        self.formula1 = 'HYPERLINK("http://a.com"\r)'
+        self.formula2 = 'HYPERLINK("http://a.com", "测试"\r)'
 
     def test_hyperlink(self):
-        formula = xlsext.hyperlink(self.link_location)
-        assert isinstance(formula, xlwt.ExcelFormula.Formula)
-        formula = xlsext.hyperlink(self.link_location, self.friendly_name)
-        assert isinstance(formula, xlwt.ExcelFormula.Formula)
+        formula1 = xlsext.hyperlink(self.link_location)
+        assert isinstance(formula1, xlwt.ExcelFormula.Formula)
+        assert formula1.text() == self.formula1
+        formula2 = xlsext.hyperlink(self.link_location, self.friendly_name)
+        assert isinstance(formula2, xlwt.ExcelFormula.Formula)
+        assert formula2.text() == self.formula2
--- xlsext/extensions.py
+++ xlsext/extensions.py
@@ -5,7 +5,9 @@ import xlwt
 
 class XlsExtTools(object):
     def hyperlink(self, link_location: str, friendly_name: str = None) -> str:
-        return xlwt.Formula('HYPERLINK("{0}", "{1}")'.format(link_location, friendly_name))
+        if not friendly_name:
+            return xlwt.Formula('HYPERLINK("{0}"\r)'.format(link_location))
+        return xlwt.Formula('HYPERLINK("{0}", "{1}"\r)'.format(link_location, friendly_name))
 
 
 xlsext = XlsExtTools()
